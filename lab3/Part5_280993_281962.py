2c9407e00cd3ea550f968687b3e999193ededfb2
sanadhis <sanadhis@gmail.com>
2017-11-03 19:08:41 +0100

    Fix Part5, ws is not blocking socket

--- lab3/Part5_280993_281962.py
+++ lab3/Part5_280993_281962.py
@@ -7,9 +7,17 @@ PORT = "5006"
 # Init connection
 ws = create_connection("ws://"+HOST+":"+PORT)
 
-#d = input("enter d: ")
-#message = "CMD_short:"+d
-message = "CMD_floodme"
+# Make sure user can choose scenarios
+option      = int(input("Enter 1 for CMD_short and 2 for CMD_floodme: "))
+
+# For scenario 1, ask user to input d
+# We set msg_size = 18 to handle lines easily
+if option == 1:
+    d        = input("enter d: ")
+    message  = "CMD_short:"+d
+# For scenario we want the application to receive up to 1024 bytes at once from the socket
+else:
+    message  = "CMD_floodme"
 
 # Send the message
 print("sending:", message)
@@ -21,10 +29,10 @@ counter=0
 # Handle the response
 while True:
     try:
-        result =  ws.recv().decode()        
+        result =  ws.recv()        
         if result:
             counter+=1            
-            print ('received:', result)
+            print ('received:', result.decode())
         else:
             break
     except:
